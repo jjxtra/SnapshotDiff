5210eb72c70bc4ffdb4143273fc247b45332e927
Jeff Johnson <jjxtra@gmail.com>
2019-08-05 11:53:00 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # SnapshotDiff
-Get email notifications when a snapshot of a url changes (image compare)
+Get email notifications when a snapshot of a url changes (image compare).
+
+## Install
+First install chrome, latest version. Then run as normal. A self contained build will ensure .NET core is not required.
+
+## Arguments
+
+Url - the url to ping
+FileName - the file name to save the current image to
+Width - chrome browser width
+Height - chrome browser height
+LoadDelayMilliseconds - time to wait for page load complete event
+ForceDelayMilliseconds - force sleep this amount of time after page load event
+Percent - the percent the pixels must change to notify
+EmailTestOnly - whether to just send a test email
+EmailHost - email server address
+EmailPort - email server port
+EmailUserName - email server user name to login with
+EmailPassword - email password to login with
+EmailFromAddress - email from address to send emails from
+EmailFromName - email from name
+EmailToAddress - email to address
+EmailSubject - email subject, {0} will be replaced with the url
---
+++ SnapshotDiffApp.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using MailKit;
+using MailKit.Net;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace SnapshotDiff
+{
+    public class SnapshotDiffOptions
+    {
+        public SnapshotDiffOptions(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                return;
+            }
+
+            foreach (string arg in args)
+            {
+                int pos = arg.IndexOf('=');
+                if (pos < 0)
+                {
+                    throw new ArgumentException("Invalid argument " + arg);
+                }
+                string key = arg.Substring(0, pos).Trim('-', '/');
+                string value = arg.Substring(++pos);
+                bool found = false;
+                foreach (PropertyInfo prop in GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                {
+                    if (prop.Name.Equals(key, StringComparison.OrdinalIgnoreCase))
+                    {
+                        found = true;
+                        prop.SetValue(this, Convert.ChangeType(value, prop.PropertyType));
+                        break;
+                    }
+                }
+                if (!found)
+                {
+                    throw new ArgumentException("Unrecognized argument " + arg);
+                }
+            }
+            FileName = Path.GetFileNameWithoutExtension(FileName) + "." + FileFormat.ToString();
+        }
+
+        public void PrintUsage()
+        {
+            Console.Write("Usage: {0} ", Path.GetFileName(System.AppDomain.CurrentDomain.FriendlyName));
+            foreach (PropertyInfo prop in GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                Console.Write("{0}={1}", prop.Name, prop.GetValue(this));
+            }
+            Console.WriteLine();
+        }
+
+        public string Url { get; private set; } = "https://www.digitalruby.com";
+        public string FileName { get; private set; }
+        public int Width { get; private set; } = 1280;
+        public int Height { get; private set; } = 1024;
+        public int LoadDelayMilliseconds { get; private set; } = 1000;
+        public int ForceDelayMilliseconds { get; private set; } = 0;
+        public float Percent { get; private set; } = 0.1f;
+        public int LoopDelaySeconds { get; private set; } = 300;
+        public ScreenshotImageFormat FileFormat { get; private set; } = ScreenshotImageFormat.Png;
+
+        public bool EmailTestOnly { get; private set; }
+        public string EmailHost { get; private set; }
+        public int EmailPort { get; private set; }
+        public string EmailUserName { get; private set; }
+        public string EmailPassword { get; private set; }
+        public string EmailFromAddress { get; private set; }
+        public string EmailFromName { get; private set; } = "SnapshotDiff";
+        public string EmailToAddress { get; private set; }
+        public string EmailSubject { get; private set; } = "Url changed! {0}";
+    }
+
+    public class SnapshotDiffApp
+    {
+        private readonly SnapshotDiffOptions options;
+        private bool running = true;
+
+        private void SendNotificationEmail(byte[] image)
+        {
+            SmtpClient client = new SmtpClient();
+            BodyBuilder builder = new BodyBuilder
+            {
+                TextBody = "Url changed: " + options.Url + "\n"
+            };
+            builder.Attachments.Add(options.FileName, image, new ContentType("image", "png"));
+            client.Connect(options.EmailHost, options.EmailPort, MailKit.Security.SecureSocketOptions.Auto);
+            client.Authenticate(options.EmailUserName, options.EmailPassword);
+            client.Send(new MimeMessage(new MailboxAddress[] { new MailboxAddress(options.EmailFromName, options.EmailFromAddress) },
+                new MailboxAddress[] { new MailboxAddress(options.EmailToAddress) },
+                string.Format(options.EmailSubject, options.Url), builder.ToMessageBody()));
+        }
+
+        public SnapshotDiffApp(SnapshotDiffOptions options)
+        {
+            this.options = options;
+        }
+
+        public int Run()
+        {
+            if (options.EmailTestOnly)
+            {
+                SendNotificationEmail(new byte[0]);
+                return 0;
+            }
+            Console.WriteLine("Press Ctrl-C to terminate");
+            Console.CancelKeyPress += Console_CancelKeyPress;
+            ChromeOptions chromeOptions = new ChromeOptions();
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            service.SuppressInitialDiagnosticInformation = true;
+            service.HideCommandPromptWindow = true;
+            chromeOptions.AddArgument("headless");
+            chromeOptions.AddArgument("disable-gpu");
+            chromeOptions.AddArgument("hide-scrollbars");
+            using (var driver = new ChromeDriver(service, chromeOptions))
+            {
+                driver.Manage().Window.Size = new System.Drawing.Size(options.Width, options.Height);
+                driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(options.LoadDelayMilliseconds);
+                while (running)
+                {
+                    Console.Write("Pinging url {0}... ", options.Url);
+                    try
+                    {
+                        driver.Navigate().GoToUrl(options.Url);
+                        Task.Delay(options.ForceDelayMilliseconds).Wait();
+                        var screenshot = driver.GetScreenshot();
+                        string tempFile = Path.Combine(Path.GetTempPath(), "SnapshotDiffTemp.img");
+                        byte[] rawBytes = screenshot.AsByteArray;
+                        screenshot.SaveAsFile(tempFile, options.FileFormat);
+                        if (File.Exists(options.FileName))
+                        {
+                            var imgCurrent = Image.Load<Rgba32>(options.FileName);
+                            var imgNext = Image.Load<Rgba32>(rawBytes);
+                            int pixelsDiff = 0;
+                            if (imgCurrent.Width == imgNext.Width && imgCurrent.Height == imgNext.Height)
+                            {
+                                for (int y = 0; y < imgCurrent.Height; y++)
+                                {
+                                    for (int x = 0; x < imgCurrent.Width; x++)
+                                    {
+                                        if (imgCurrent.Frames[0][x, y] != imgNext.Frames[0][x, y])
+                                        {
+                                            pixelsDiff++;
+                                        }
+                                    }
+                                }
+                            }
+                            float percentDiff = (float)pixelsDiff / ((float)imgNext.Width * (float)imgNext.Height);
+                            if (percentDiff < options.Percent)
+                            {
+                                Console.WriteLine("No change.");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Url changed!");
+                                Task.Run(() =>
+                                {
+                                    try
+                                    {
+                                        SendNotificationEmail(rawBytes);
+                                    }
+                                    catch (Exception e)
+                                    {
+                                        Console.WriteLine("Failed to send notification email: {0}", e);
+                                    }
+                                });
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("First ping, saved image.");
+                        }
+                        File.Delete(options.FileName);
+                        File.Move(tempFile, options.FileName);
+                        Task.Delay(TimeSpan.FromSeconds(options.LoopDelaySeconds)).Wait();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: {0}", ex);
+                    }
+                }
+            }
+            return 0;
+        }
+
+        private void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            running = false;
+        }
+
+        public static int Main(string[] args)
+        {
+            if (args.Length == 0 || args[0].Contains("help", StringComparison.OrdinalIgnoreCase))
+            {
+                new SnapshotDiffOptions(null).PrintUsage();
+                return 1;
+            }
+            return new SnapshotDiffApp(new SnapshotDiffOptions(args)).Run();
+        }
+    }
+}
